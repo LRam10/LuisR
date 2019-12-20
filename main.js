7ec8cbc98d84161ee349572fb6782ee3d3f760e9
LuisRamirez <Lramirez@na.edu>
2019-12-20 15:47:31 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+//Show ecommerce techn
+document.addEventListener("DOMContentLoaded",()=>{
+let projectList = document.querySelectorAll('.card-img-top');
+console.log(projectList);
+projectList.forEach(element => {
+    element.addEventListener('mousedown',()=>{
+        element.style.opacity = '-.08';
+        element.previousElementSibling.classList.remove('d-none');
+    });
+    element.addEventListener('mouseout',()=>{
+        element.style.opacity = '1';
+        element.previousElementSibling.classList.add('d-none');
+    });
+});
+
+
+
+});
