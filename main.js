4c808c9a40389fdd6bbb0797a5da43deb32cb521
luis <luis@na.edu>
2022-08-30 14:03:56 -0500

    new design and layou

--- main.js
+++ main.js
@@ -1,18 +1,19 @@
 //Show ecommerce techn
-document.addEventListener("DOMContentLoaded",()=>{
-let projectList = document.querySelectorAll('.card-img-top');
-console.log(projectList);
-projectList.forEach(element => {
-    element.addEventListener('mousedown',()=>{
-        element.style.opacity = '-.08';
-        element.previousElementSibling.classList.remove('d-none');
+document.addEventListener("DOMContentLoaded", () => {
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip()
+      })
+
+  let projectList = document.querySelectorAll(".card-img-top");
+  console.log(projectList);
+  projectList.forEach((element) => {
+    element.addEventListener("mousedown", () => {
+      element.style.opacity = "-.08";
+      element.previousElementSibling.classList.remove("d-none");
     });
-    element.addEventListener('mouseout',()=>{
-        element.style.opacity = '1';
-        element.previousElementSibling.classList.add('d-none');
+    element.addEventListener("mouseout", () => {
+      element.style.opacity = "1";
+      element.previousElementSibling.classList.add("d-none");
     });
-});
-
-
-
+  });
 });
