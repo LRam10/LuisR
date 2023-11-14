00ee207e9e91b33849848127cdade673a60a4bfb
luis <luis@na.edu>
2023-11-13 21:07:07 -0600

    change fonts and shorter about me section

--- main.js
+++ main.js
@@ -4,9 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
       let about_anchor = anchor_list[0];
       about_anchor.addEventListener('click', (e)=>{
         e.preventDefault();
-        const section  = document.getElementById('about');
-        console.log(section.clientHeight,section.scrollWidth)
-        
+        const section  = document.getElementById('about'); 
         window.scrollTo({
           top:section.offsetHeight
         })
